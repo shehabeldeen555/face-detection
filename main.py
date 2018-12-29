1f301d63bfd74a13098a607eca39c3406b318e53
Shehabeldeen <shehabeldeen555@gmail.com>
2018-12-29 23:08:54 +0200

    add name

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import cv2 as cv
 import numpy as np
 import tensorflow as tf
+
 names = ['ashraf', 'joseph', 'magdy', 'ref', 'sayed', 'Shehab']
 model = tf.keras.models.load_model('best-conv.h5')
-
 
 face_cascade = cv.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 cap = cv.VideoCapture(0)
-i = 0
 while 1:
     ret, img = cap.read()
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -19,19 +18,14 @@ while 1:
         w = w + 60
         h = h + 60
         cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-
         face = img[y:y + h, x:x + w]
         face = cv.resize(face, (48, 48))
-
         face_to_predict = np.zeros(shape=(1, 48, 48, 3))
         face_to_predict[0] = face
-        print(names[np.argmax(model.predict(face_to_predict))])
+        name = names[np.argmax(model.predict(face_to_predict))]
+        cv.putText(img, name, (x, y), cv.FONT_HERSHEY_SIMPLEX, 0.8, (0, 125, 255), 2, cv.LINE_AA)
 
-
-        # cv.imwrite("faces/face%d.jpg" % i, face)
-        i += 1
-
-    img = cv.flip(img, 1)
+    # img = cv.flip(img, 1)
     cv.imshow('img', img)
     k = cv.waitKey(30) & 0xff
     if k == 27:
